5374ee32796cf84b03d279d5fb505c1ade441280
Brian Boudrioux <brian.boudrioux@wildcodeschool.com>
2025-01-02 15:56:54 +0100

    add schema.sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,99 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
-);
+-- MySQL Workbench Forward Engineering
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
+-- -----------------------------------------------------
+-- Schema tuneApp
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `tuneApp` ;
 
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+-- -----------------------------------------------------
+-- Schema tuneApp
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `tuneApp` DEFAULT CHARACTER SET utf8 ;
+USE `tuneApp` ;
+
+-- -----------------------------------------------------
+-- Table `tuneApp`.`User`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tuneApp`.`User` (
+  `id` INT NOT NULL,
+  `email` VARCHAR(125) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tuneApp`.`Album`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tuneApp`.`Album` (
+  `id` INT NOT NULL,
+  `title` VARCHAR(85) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `User_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Album_User_idx` (`User_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Album_User`
+    FOREIGN KEY (`User_id`)
+    REFERENCES `tuneApp`.`User` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tuneApp`.`Track`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tuneApp`.`Track` (
+  `id` INT NOT NULL,
+  `title` VARCHAR(85) NOT NULL,
+  `Album_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Track_Album1_idx` (`Album_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Track_Album1`
+    FOREIGN KEY (`Album_id`)
+    REFERENCES `tuneApp`.`Album` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tuneApp`.`Artist`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tuneApp`.`Artist` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(125) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tuneApp`.`Artist_has_Track`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tuneApp`.`Artist_has_Track` (
+  `Artist_id` INT NOT NULL,
+  `Track_id` INT NOT NULL,
+  PRIMARY KEY (`Artist_id`, `Track_id`),
+  INDEX `fk_Artist_has_Track_Track1_idx` (`Track_id` ASC) VISIBLE,
+  INDEX `fk_Artist_has_Track_Artist1_idx` (`Artist_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Artist_has_Track_Artist1`
+    FOREIGN KEY (`Artist_id`)
+    REFERENCES `tuneApp`.`Artist` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Artist_has_Track_Track1`
+    FOREIGN KEY (`Track_id`)
+    REFERENCES `tuneApp`.`Track` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;

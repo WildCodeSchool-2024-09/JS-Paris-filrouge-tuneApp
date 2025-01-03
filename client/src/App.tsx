90380d1ab92b108b79da7d887f2f322b1338e9d0
Brian Boudrioux <brian.boudrioux@wildcodeschool.com>
2025-01-03 16:56:21 +0100

    add dashboard page and create albumForm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header></header>
+      <nav></nav>
+      <Outlet />
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/AlbumForm.tsx
@@ -0,0 +1,11 @@
+function AlbumForm({setTitle, submitAlbum}) {
+
+  return (
+    <form onSubmit={submitAlbum}>
+      <input type="text" onInput={(e) => setTitle(e.target.value)}  placeholder="Entrez un titre d'album"/>
+      <button type="submit">Ajouter</button>
+    </form>
+  )
+}
+
+export default AlbumForm;

f9d6db2283b8d1306cac46bb4060ae88635ccd40
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2025-01-09 16:34:32 +0100

    Add repository function to get an album

--- server/src/modules/album/AlbumRepository.ts
+++ server/src/modules/album/AlbumRepository.ts
@@ -9,6 +9,10 @@ type Album = {
 
 class AlbumRepository {
 
+  readAlbumById(id: number) {
+    return db.query("SELECT * FROM album WHERE id = ?", [id]);
+  }
+
   createAlbum(album : Album) {
     const { title, user_id} = album;
     return db.query("INSERT INTO album (title, user_id) VALUES (?, ?)", [title, user_id]);
